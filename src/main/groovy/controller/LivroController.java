babce72da45e00f4a8c4ba274465c3aee5153511
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2025-11-09 16:22:03 -0400

    feat: add controllers to handle view requests

--- src/main/groovy/controller/LivroController.java
+++ src/main/groovy/controller/LivroController.java
@@ -1,4 +1,28 @@
 package controller;
 
+import model.LivroModel;
+import repository.LivroRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class LivroController {
+    private LivroRepository livroRepository = new LivroRepository();
+
+    public String salvar(LivroModel livro) throws SQLException {
+        return livroRepository.salvar(livro);
+    }
+
+    public String editar(LivroModel livro) throws SQLException {
+        return livroRepository.editar(livro);
+    }
+
+    public String excluir(Long idLivro) throws SQLException {
+        LivroModel livro = livroRepository.buscarPorId(idLivro);
+        return livroRepository.remover(livro);
+    }
+
+    public List<LivroModel> listar() throws SQLException {
+        return livroRepository.buscarTodos();
+    }
 }
--- src/main/groovy/controller/UsuarioController.java
+++ src/main/groovy/controller/UsuarioController.java
@@ -1,4 +1,28 @@
 package controller;
 
+import model.UsuarioModel;
+import repository.UsuarioRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class UsuarioController {
+    private UsuarioRepository usuarioRepository = new UsuarioRepository();
+
+    public String salvar(UsuarioModel usuario) throws SQLException {
+        return usuarioRepository.salvar(usuario);
+    }
+
+    public String editar(UsuarioModel usuario) throws SQLException {
+        return usuarioRepository.editar(usuario);
+    }
+
+    public String excluir(Long idUsuario) throws SQLException {
+        UsuarioModel usuario = usuarioRepository.buscarPorId(idUsuario);
+        return usuarioRepository.remover(usuario);
+    }
+
+    public List<UsuarioModel> listar() throws SQLException {
+        return usuarioRepository.buscarTodos();
+    }
 }
